f3ddb7d055658195b116d756d3e24e3283130b25
leebri101 <>
2023-09-26 11:23:21 +0000

    Cosmetic changes to article model

--- articles/models.py
+++ articles/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 # Article model strutcture
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
-    body = models.TextField()
+    content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    pic = models.ImageField(default='default.png', blank=True)
-    author = models.ForeignKey(User, default=None)
+    featured_image = CloudinaryField('image', default='placeholder')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
 
     def __str__(self):
         return self.title
 
-    def snippet(self):
-        return self.body[:50] + '...'
+    def number_of_likes(self):
+        return self.likes.count()
