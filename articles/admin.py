3b27038197abce6ec7a7ff21cc8d938516de4b79
leebri101 <>
2023-09-26 17:11:19 +0000

    Change of model structure

--- articles/admin.py
+++ articles/admin.py
@@ -1,29 +1,9 @@
 from django.contrib import admin
-from .models import Article, Comment
-# If you are using django_summernote for Article content
+from .models import Article
 from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
-    # Valid attributes of Article
-    list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ('title', 'content')  # Valid attributes of Article
-    list_filter = ('status', 'created_on')  # Valid attributes of Article
-    prepopulated_fields = {'slug': ('title',)}
-    # Enable Summernote for the content field (if using django_summernote)
-    summernote_fields = ('content',)
 
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'content', 'article', 'created_on',
-                    'approved')  # Valid attributes of Comment
-    list_filter = ('approved', 'created_on')  # Valid attributes of Comment
-    search_fields = ('name', 'content')  # Valid attributes of Comment
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
-
-
-admin.site.register(Article, ArticleAdmin)
-admin.site.register(Comment, CommentAdmin)
+    summernote_fields = ('content')
