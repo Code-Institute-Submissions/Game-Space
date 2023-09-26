01f55af6ad7edf41427a452be8a10b8a6fbf1083
leebri101 <>
2023-09-26 19:02:55 +0000

    Changes in class structure

--- articles/admin.py
+++ articles/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+# Article admin display.
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
+    # full list display of entities
+    list_display = ('title', 'slug', 'author', 'created_on',
+                    'status', 'number_of_likes')
+    # Filters
+    list_filter = ('status', 'created_on', 'author')
+    # Serach fields
+    search_fields = ('title', 'content', 'excerpt')
+    prepopulated_fields = {'slug': ('title',)}
+    # Summer note custom text field
+    summernote_fields = ('content',)
 
-    summernote_fields = ('content')
+    def number_of_likes(self, obj):
+        # likes column
+        return obj.likes.count()
+    number_of_likes.short_description = 'Likes'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # full list display of entities
+    list_display = ('name', 'article', 'user', 'created_on', 'approved')
+    # Filters
+    list_filter = ('approved', 'created_on', 'user')
+    # Serach fields
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- articles/models.py
+++ articles/models.py
@@ -3,20 +3,26 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from cloudinary.models import CloudinaryField
 
-
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+# Article attributes
 class Article(models.Model):
+    STATUS_CHOICES = (
+        (0, "Draft"),
+        (1, "Published")
+    )
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="article_posts")
+        User, on_delete=models.CASCADE, related_name="article_posts", related_query_name="article_post"
+    )
     image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(default=timezone.now)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
     likes = models.ManyToManyField(
         User, related_name='articlepost_like', blank=True)
 
@@ -30,11 +36,12 @@ class Article(models.Model):
         return self.likes.count()
 
 
+# Comment attributes
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE,
                                 related_name="comments")
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(default=timezone.now)
     approved = models.BooleanField(default=False)
