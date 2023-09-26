fd599565f1fb0617c0f8b0934155c40b5061c868
leebri101 <>
2023-09-26 12:00:35 +0000

    Grammatical changes to main file url

--- gamespace/urls.py
+++ gamespace/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, inlude
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', include('articles.url')),
+    path("", include('articles.url'), name="articles-urls"),
+    path('summernote/', include('django_summernote.urls')),
     path('about/', views.about),
 ]
