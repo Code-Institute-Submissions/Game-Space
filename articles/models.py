bc76a5fe11f0b4e3a24f70ffdf9dbe9cd6bafb62
leebri101 <>
2023-09-26 13:22:27 +0000

    Cosmetic code changes to ensure code now loads

--- articles/models.py
+++ articles/models.py
@@ -1,44 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 
-STATUS = ((0, "Draft"), (1, "Posted"))
-
-
-# Article model strutcture
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    featured_image = CloudinaryField('image', default='placeholder')
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
-    excerpt = models.TextField(blank=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+    created_on = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
 
-    def number_of_likes(self):
-        return self.likes.count()
-
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE,
-                                related_name="comments")
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
-    email = models.EmailField()
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(default=timezone.now)
     approved = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ["created_on"]
-
     def __str__(self):
-        return f"Comment {self.content} by {self.name}"
+        return f'Comment by {self.name} on {self.article}'
