5f411ac6c051dedd69c2f319c5e3f5d1bb63a2bc
leebri101 <>
2023-09-26 12:18:24 +0000

    Intial migration of articles

--- gamespace/urls.py
+++ gamespace/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('articles.url'), name="articles-urls"),
-    path('summernote/', include('django_summernote.urls')),
-    path('about/', views.about),
+    #path("", include('articles'), name="articles"),
+    #path('summernote/', include('django_summernote.urls')),
+    #path('about/', views.about),
 ]
