3396edc862b3cfc8bb91fb2551f4512ce5799fe7
leebri101 <>
2023-09-27 10:10:31 +0000

    Cosmetic changes to structure

--- articles/urls.py
+++ articles/urls.py
@@ -2,15 +2,13 @@ from . import views
 from django.urls import path
 
 
-app_name = 'articles'
-
 urlpatterns = [
-    # Define URL patterns for articles
-    #path('articles/', views.ArticleListView.as_view(), name='article_list'),
-    #path('articles/<slug:slug>/',
-         #views.ArticleDetailView.as_view(), name='article_detail'),
+    # URL patterns for viewing on views.py
+    # path('articles/', views.ArtoclesListView.as_view(), name='article_list'),
+    # path('articles/<slug:slug>/', 
+    # views.ArticleDetailView.as_view(), name='article_detail'),
 
-    # Define URL patterns for comments (optional)
-    #path('articles/<slug:slug>/comments/',
-         #views.CommentCreateView.as_view(), name='comment_create'),
+    # Comments section for URL viewing
+    # path('articles/<slug:slug>/comments/',
+    # views.CommentCreateView.as_view(), name='comment_create'),
 ]
