d306b7876ec26ef327f794f721e895e78958f162
leebri101 <>
2023-09-28 21:41:00 +0000

    Chnages in template tags

--- articles/views.py
+++ articles/views.py
@@ -17,8 +17,7 @@ class ArticleDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Article.objects.filter(status=1)
         article = get_object_or_404(queryset, slug=slug)
-        comments = article.comments.filter(
-            approved=True).order_by("-created_on")
+        comments = article.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if article.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -39,13 +38,11 @@ class ArticleDetail(View):
 
         queryset = Article.objects.filter(status=1)
         article = get_object_or_404(queryset, slug=slug)
-        comments = article.comments.filter(
-            approved=True).order_by("-created_on")
+        comments = article.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if article.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
-        comment_form = CommentForm(data=request.ARTICLE)
+            liked = True 
+            comment_form = CommentForm(data=request.ARTICLE)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
--- gamespace/settings.py
+++ gamespace/settings.py
@@ -21,6 +21,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -84,7 +85,10 @@ ROOT_URLCONF = 'gamespace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
